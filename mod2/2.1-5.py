abf1ad5dcc5daf1b72a7b8b09c57e7944a21de55
dm <d.metro@inbox.ru>
2023-12-13 23:05:46 +0300

    added "click on checkboxes and radiobuttons (captcha for robots)"

---
+++ mod2/2.1-5.py
@@ -0,0 +1,30 @@
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "https://suninjuly.github.io/math.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+x_element = browser.find_element(By.ID, "input_value")
+x = x_element.text
+y = calc(x)
+
+answer = browser.find_element(By.ID, "answer")
+answer.send_keys(y)
+
+check = browser.find_element(By.ID, "robotCheckbox")
+check.click()
+
+radio = browser.find_element(By.ID, "robotsRule")
+radio.click()
+
+button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+button.click()
+
+time.sleep(5)
+webdriver.Chrome().quit()
